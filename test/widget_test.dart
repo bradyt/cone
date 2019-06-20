9340a95f1d3c7860671deebcd22899ce34d52b2c
Brady Trainor <mail@bradyt.com>
2019-06-19 19:38:53 -0700

    Quiet dart analyze at widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cone/main.dart';
+import 'package:cone/src/flutter/cone_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ConeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
