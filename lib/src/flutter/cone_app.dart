80d6c830ae8c0d007fb6f7a265f632bb80a36c55
Brady Trainor <mail@bradyt.com>
2019-06-19 21:37:21 -0700

    Fix thrown exception
    Fixes a thrown exception disrupting flutter run and hot reload. The
    first three lines of the exception are as follows.
    
        ══╡ EXCEPTION CAUGHT BY FOUNDATION LIBRARY ╞═════════════════════════
        The following assertion was thrown while dispatching notifications for SettingsModel:
        setState() or markNeedsBuild() called during build.
    
    A summary of changes:
    
      - ChangeNotifierProvider is now an ancestor of FutureBuilder, rather
        than descendant of.
    
      - Default values in settings are now initialized in a Future in an
        initState, rather than in a getter in SettingsModel.
    
    This refactor was largely in thanks to discussion with Randal
    L. Schwartz, but any mistakes are my own.

--- lib/src/flutter/cone_app.dart
+++ lib/src/flutter/cone_app.dart
@@ -4,37 +4,60 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:cone/src/flutter/add_transaction.dart';
 import 'package:cone/src/flutter/home.dart';
-import 'package:cone/src/flutter/settings_model.dart';
 import 'package:cone/src/flutter/settings.dart';
+import 'package:cone/src/flutter/settings_model.dart';
 
 class ConeApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => SettingsModel(),
+      child: ConeSettings(),
+    );
+  }
+}
+
+class ConeSettings extends StatefulWidget {
+  ConeSettingsState createState() => ConeSettingsState();
+}
+
+class ConeSettingsState extends State<ConeSettings> {
+  Future<SharedPreferences> sharedPreferences;
+
+  initState() {
+    super.initState();
+    sharedPreferences = SharedPreferences.getInstance().then(
+      (SharedPreferences prefs) {
+        Provider.of<SettingsModel>(context).sharedPreferences = prefs;
+        Provider.of<SettingsModel>(context).defaultCurrency ??= 'USD';
+        Provider.of<SettingsModel>(context).defaultAccountOne ??=
+            'expenses:miscellaneous';
+        Provider.of<SettingsModel>(context).defaultAccountTwo ??=
+            'assets:checking';
+      },
+    );
+  }
+
+  Widget build(BuildContext context) {
     return FutureBuilder(
-      future: SharedPreferences.getInstance(),
+      future: sharedPreferences,
       builder:
           (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
           case ConnectionState.active:
           case ConnectionState.waiting:
-            return Container();
+            return Center(child: CircularProgressIndicator());
           case ConnectionState.done:
-            return ChangeNotifierProvider(
-              builder: (context) => SettingsModel(snapshot.data),
-              child: MaterialApp(
-                title: 'cone',
-                debugShowCheckedModeBanner: false,
-                initialRoute: '/',
-                theme: ThemeData(
-                  primarySwatch: Colors.green,
-                  accentColor: Colors.amberAccent,
-                ),
-                routes: {
-                  '/': (context) => Home(),
-                  '/add-transaction': (context) => AddTransaction(),
-                  '/settings': (context) => Settings(),
-                },
+            return MaterialApp(
+              theme: ThemeData(
+                primarySwatch: Colors.green,
+                accentColor: Colors.amberAccent,
               ),
+              routes: {
+                '/': (context) => Home(),
+                '/add-transaction': (context) => AddTransaction(),
+                '/settings': (context) => Settings(),
+              },
             );
         }
       },
--- lib/src/flutter/settings_model.dart
+++ lib/src/flutter/settings_model.dart
@@ -2,71 +2,42 @@ import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsModel extends ChangeNotifier {
-  SharedPreferences prefs;
-
   String _defaultCurrency;
   String _defaultAccountOne;
   String _defaultAccountTwo;
+  SharedPreferences prefs;
 
-  SettingsModel(this.prefs);
+  set sharedPreferences(SharedPreferences preferences) {
+    prefs = preferences;
+  }
 
   String get defaultCurrency {
-    if (_defaultCurrency == null) {
-      _defaultCurrency = prefs.getString('default_currency');
-      if (_defaultCurrency == null) {
-        _defaultCurrency = 'USD';
-        prefs.setString('default_currency', _defaultCurrency);
-      }
-      notifyListeners();
-    }
     return _defaultCurrency;
   }
 
   set defaultCurrency(String defaultCurrency) {
-    if (defaultCurrency != _defaultCurrency) {
-      prefs.setString('default_currency', defaultCurrency);
-      _defaultCurrency = defaultCurrency;
-      notifyListeners();
-    }
+    prefs.setString('default_currency', defaultCurrency);
+    _defaultCurrency = defaultCurrency;
+    notifyListeners();
   }
 
   String get defaultAccountOne {
-    if (_defaultAccountOne == null) {
-      _defaultAccountOne = prefs.getString('default_account_one');
-      if (_defaultAccountOne == null) {
-        _defaultAccountOne = 'expenses:miscellaneous';
-        prefs.setString('default_account_one', _defaultAccountOne);
-      }
-      notifyListeners();
-    }
     return _defaultAccountOne;
   }
 
   set defaultAccountOne(String defaultAccountOne) {
-    if (defaultAccountOne != _defaultAccountOne) {
-      prefs.setString('default_account_one', defaultAccountOne);
-      _defaultAccountOne = defaultAccountOne;
-      notifyListeners();
-    }
+    prefs.setString('default_account_one', defaultAccountOne);
+    _defaultAccountOne = defaultAccountOne;
+    notifyListeners();
   }
 
   String get defaultAccountTwo {
-    if (_defaultAccountTwo == null) {
-      _defaultAccountTwo = prefs.getString('default_account_two');
-      if (_defaultAccountTwo == null) {
-        _defaultAccountTwo = 'assets:checking';
-        prefs.setString('default_account_two', _defaultAccountTwo);
-      }
-      notifyListeners();
-    }
     return _defaultAccountTwo;
   }
 
   set defaultAccountTwo(String defaultAccountTwo) {
-    if (defaultAccountTwo != _defaultAccountTwo) {
-      prefs.setString('default_account_two', defaultAccountTwo);
-      _defaultAccountTwo = defaultAccountTwo;
-      notifyListeners();
-    }
+    prefs.setString('default_account_two', defaultAccountTwo);
+    _defaultAccountTwo = defaultAccountTwo;
+    notifyListeners();
   }
 }
