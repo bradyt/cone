fd4f532be906e06846da8a5ab77d8985647c49a8
Brady Trainor <mail@bradyt.com>
2019-07-29 03:09:32 -0700

    Move methods from cone app to uri_picker

--- android/src/main/java/info/tangential/uri_picker/UriPickerPlugin.java
+++ android/src/main/java/info/tangential/uri_picker/UriPickerPlugin.java
@@ -43,7 +43,26 @@ public class UriPickerPlugin implements MethodCallHandler, ActivityResultListene
     if (call.method.equals("pickUri")) {
       performFileSearch();
     } else {
-      result.notImplemented();
+      Uri uri = Uri.parse(call.argument("uri"));
+      switch(call.method) {
+        case "getDisplayName":
+          getDisplayName(uri);
+          break;
+        case "isUriOpenable":
+          isUriOpenable(uri);
+          break;
+        case "takePersistablePermission":
+          takePersistablePermission(uri);
+          break;
+        case "readTextFromUri":
+          readTextFromUri(uri);
+          break;
+        case "alterDocument":
+          alterDocument(uri, call.argument("newContents"));
+          break;
+        default:
+          result.notImplemented();
+      }
     }
   }
 
@@ -51,7 +70,10 @@ public class UriPickerPlugin implements MethodCallHandler, ActivityResultListene
     Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
     intent.addCategory(Intent.CATEGORY_OPENABLE);
     intent.setType("text/plain");
-    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    intent.setFlags(
+        Intent.FLAG_GRANT_READ_URI_PERMISSION
+            | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
     registrar.activity().startActivityForResult(intent, OPEN_REQUEST_CODE);
   }
 
@@ -67,5 +89,123 @@ public class UriPickerPlugin implements MethodCallHandler, ActivityResultListene
     result.error("onActivityResult",
         "Unhandled request code, or null result data", null);
     return true;
+  }
+
+  public void getDisplayName(Uri uri) {
+    String displayName = null;
+    Cursor cursor = this.getContentResolver().query(uri, null, null, null, null, null);
+    try {
+      if (cursor != null && cursor.moveToFirst()) {
+        displayName = cursor.getString(
+          cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+      }
+    } finally {
+      cursor.close();
+    }
+    channelResult.success(displayName);
+  }
+
+  private void isUriOpenable(Uri uri) {
+    InputStream inputStream = null;
+    String name = null;
+    String message = null;
+    try {
+      inputStream = this.getContentResolver().openInputStream(uri);
+    } catch (FileNotFoundException e) {
+      name = e.getClass().getName();
+      message = e.getMessage();
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+        }
+      }
+      if (name == null) {
+        channelResult.success(null);
+      } else {
+        channelResult.error(name, message, null);
+      }
+    }
+  }
+
+  private void takePersistablePermission(Uri uri) {
+    try {
+      this.getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    } catch(Exception e) {
+      e.printStackTrace();
+    } finally {
+      channelResult.success(null);
+    }
+  }
+
+
+  public void readTextFromUri(Uri uri) {
+    InputStream inputStream = null;
+    StringBuilder stringBuilder = new StringBuilder();
+    String name = null;
+    String message = null;
+    try {
+      inputStream = this.getContentResolver().openInputStream(uri);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        stringBuilder.append(line + "\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+
+        }
+      }
+    }
+    if (name == null) {
+      channelResult.success(stringBuilder.toString());
+    } else {
+      channelResult.error(name, message, null);
+    }
+  }
+
+  private void alterDocument(Uri uri, String newContents) {
+    String name = null;
+    String message = null;
+    ParcelFileDescriptor pfd = null;
+    FileOutputStream fileOutputStream = null;
+    try {
+      pfd = this.getContentResolver().openFileDescriptor(uri, "w");
+      fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
+      try {
+        fileOutputStream.write((newContents).getBytes());
+      } catch (IOException e) {
+        name = e.getClass().getName();
+        message = e.getMessage();
+      } finally {
+        try {
+          fileOutputStream.close();
+        } catch (IOException e) {
+          name = e.getClass().getName();
+          message = e.getMessage();
+        }
+      }
+    } catch (FileNotFoundException e) {
+      name = e.getClass().getName();
+      message = e.getMessage();
+    } finally {
+      try {
+        pfd.close();
+      } catch (IOException e) {
+        name = e.getClass().getName();
+        message = e.getMessage();
+      }
+    }
+    if (name == null) {
+      channelResult.success(null);
+    } else {
+      channelResult.error(name, message, null);
+    }
   }
 }
--- lib/uri_picker.dart
+++ lib/uri_picker.dart
@@ -10,4 +10,82 @@ class UriPicker {
     final String uri = await _channel.invokeMethod('pickUri');
     return uri;
   }
+
+  static Future<String> pickUri() async {
+    String uri;
+    try {
+      uri = await _channel.invokeMethod<dynamic>('pickUri') as String;
+    } on PlatformException {
+      rethrow;
+    } on MissingPluginException {
+      rethrow;
+    }
+    return uri;
+  }
+
+  static Future<String> getDisplayName(String uri) async {
+    String displayName;
+    try {
+      displayName = await _channel
+          .invokeMethod<dynamic>('getDisplayName', <String, String>{
+        'uri': uri,
+      }) as String;
+    } on PlatformException {
+      rethrow;
+    }
+    return displayName;
+  }
+
+  static Future<void> isUriOpenable(String uri) async {
+    try {
+      await _channel.invokeMethod<dynamic>('isUriOpenable', <String, String>{
+        'uri': uri,
+      });
+    } on PlatformException {
+      rethrow;
+    } on MissingPluginException {
+      rethrow;
+    }
+  }
+
+  static Future<void> takePersistablePermission(String uri) async {
+    try {
+      await _channel
+          .invokeMethod<dynamic>('takePersistablePermission', <String, String>{
+        'uri': uri,
+      });
+    } on PlatformException {
+      rethrow;
+    } on MissingPluginException {
+      rethrow;
+    }
+  }
+
+  static Future<String> readFile(String uri) async {
+    String fileContents;
+    try {
+      fileContents = await _channel
+          .invokeMethod<dynamic>('readTextFromUri', <String, String>{
+        'uri': uri,
+      }) as String;
+    } on PlatformException {
+      rethrow;
+    }
+    return fileContents;
+  }
+
+  static Future<void> appendFile(String uri, String contentsToAppend) async {
+    await isUriOpenable(uri);
+    final String originalContents = await readFile(uri);
+    final String newContents =
+        combineContentsWithLinebreak(originalContents, contentsToAppend);
+    try {
+      await _channel.invokeMethod<dynamic>('alterDocument', <String, String>{
+        'uri': uri,
+        'newContents': newContents,
+      });
+    } on PlatformException catch (e) {
+      print('PlatformException $e');
+    }
+  }
 }
