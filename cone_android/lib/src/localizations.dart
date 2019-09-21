7580588836f2668c5437c766712fab6ed879769a
Brady Trainor <mail@bradyt.net>
2019-09-20 19:01:16 -0700

    Move localizedValues to localized_values.dart

--- cone_android/lib/src/localizations.dart
+++ cone_android/lib/src/localizations.dart
@@ -4,6 +4,8 @@ import 'package:flutter/foundation.dart' show SynchronousFuture;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show NumberFormat;
 
+import 'package:cone/src/localized_values.dart' show localizedValues;
+
 class ConeLocalizations {
   ConeLocalizations(this.locale);
 
@@ -27,40 +29,7 @@ class ConeLocalizations {
   }
 
   static final Map<String, Map<String, String>> _localizedValues =
-      <String, Map<String, String>>{
-    'pt': <String, String>{
-      'addTransaction': 'Adicionar transação',
-      'settings': 'Definições',
-      'defaultCurrency': 'Moeda padrão',
-      'currencyOnLeft': 'Moeda à esquerda',
-      'enterDefaultCurrency': 'Digite moeda predefinida',
-      'submit': 'Enviar',
-    },
-    'en': <String, String>{
-      'addTransaction': 'Add transaction',
-      'settings': 'Settings',
-      'defaultCurrency': 'Default Currency',
-      'currencyOnLeft': 'Currency on left',
-      'enterDefaultCurrency': 'Enter default currency',
-      'submit': 'Submit',
-    },
-    'es': <String, String>{
-      'addTransaction': 'Añadir transacción',
-      'settings': 'Ajustes',
-      'defaultCurrency': 'Moneda por defecto',
-      'currencyOnLeft': 'Moneda a la izquierda',
-      'enterDefaultCurrency': 'Introduzca la moneda por defecto',
-      'submit': 'Enviar',
-    },
-    'ru': <String, String>{
-      'addTransaction': 'Добавить транзакцию',
-      'settings': 'Настройки',
-      'defaultCurrency': 'Валюта по-умолчанию',
-      'currencyOnLeft': 'Валюта слева',
-      'enterDefaultCurrency': 'Введите валюту по-умолчанию',
-      'submit': 'Принять',
-    },
-  };
+      localizedValues;
 
   String get settings {
     return _localizedValues[locale.languageCode]['settings'];
---
+++ cone_android/lib/src/localized_values.dart
@@ -0,0 +1,35 @@
+final Map<String, Map<String, String>> localizedValues =
+    <String, Map<String, String>>{
+  'pt': <String, String>{
+    'addTransaction': 'Adicionar transação',
+    'settings': 'Definições',
+    'defaultCurrency': 'Moeda padrão',
+    'currencyOnLeft': 'Moeda à esquerda',
+    'enterDefaultCurrency': 'Digite moeda predefinida',
+    'submit': 'Enviar',
+  },
+  'en': <String, String>{
+    'addTransaction': 'Add transaction',
+    'settings': 'Settings',
+    'defaultCurrency': 'Default Currency',
+    'currencyOnLeft': 'Currency on left',
+    'enterDefaultCurrency': 'Enter default currency',
+    'submit': 'Submit',
+  },
+  'es': <String, String>{
+    'addTransaction': 'Añadir transacción',
+    'settings': 'Ajustes',
+    'defaultCurrency': 'Moneda por defecto',
+    'currencyOnLeft': 'Moneda a la izquierda',
+    'enterDefaultCurrency': 'Introduzca la moneda por defecto',
+    'submit': 'Enviar',
+  },
+  'ru': <String, String>{
+    'addTransaction': 'Добавить транзакцию',
+    'settings': 'Настройки',
+    'defaultCurrency': 'Валюта по-умолчанию',
+    'currencyOnLeft': 'Валюта слева',
+    'enterDefaultCurrency': 'Введите валюту по-умолчанию',
+    'submit': 'Принять',
+  },
+};
